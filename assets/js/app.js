96aa3eb5784ddc64731faacd13b024b6e529f8bd
hendurhance <jossyowolabi5@gmail.com>
2020-04-13 16:55:51 +0100

    done with homepage frontend

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,4 @@
 $(window).scroll(function(){
    $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
+
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width, initial-scale=1.0">
     <title>CV Builder | OctaJobs</title>
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.min.css">
     <link rel="stylesheet" href="/assets/css/app.css">
     <link rel="stylesheet" href="/assets/css/scroll-down.css">
     <link rel="stylesheet" href="/assets/css/responsive.css">
@@ -18,7 +19,7 @@
                 <img src="/assets/img/OctaJobs.svg" alt="OctaJobs Logo">
               </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <i class="fas fa-ellipsis-h fa-2x"></i>
               </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
@@ -42,7 +43,7 @@
       </nav>
       
     <!-- HEADER SECTION -->
-    <header class="masthead">
+      <header class="masthead">
         <div class="container h-100">
           <div class="row h-100 align-items-center">
             <div class="col-lg-6 col-md-6 col-sm-12">
@@ -53,24 +54,112 @@
                 <img src="/assets/img/resume.svg" class="img-header img-fluid" alt="">
             </div>
             <div class="scroll">
-  <div class="chevron"></div>
-  <div class="chevron"></div>
-  <div class="chevron"></div>
-  <span class="text">Scroll down</span>
-</div>
+           <div class="chevron"></div><div class="chevron"></div><div class="chevron"></div><span class="text">Scroll down</span></div>
           </div>
         </div>
       </header>
     
-<section>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, veniam sit! Illo id, eos ipsam libero vel repellat neque voluptas dolores molestias veritatis explicabo quidem sapiente ipsa necessitatibus laudantium harum!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, veniam sit! Illo id, eos ipsam libero vel repellat neque voluptas dolores molestias veritatis explicabo quidem sapiente ipsa necessitatibus laudantium harum!</p>
-</section>
+    <!-- BOARD SECTION -->
+      <section class="board">
+        <div class="container">
+          <div class="row board-row">
+            <div class="col-md-4 col-lg-4 col-sm-12">
+              <div class="board-main">
+                <i class="fas fa-shield-alt fas-board"></i>
+                <h6 class="board-h6">Fast &amp; Secure</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo qui obcaecati beatae minus a</p>
+              </div>
+            </div>
+            <div class="col-md-4 col-lg-4 col-sm-12">
+              <div class="board-main">
+                <div class="board-icon">
+                  <i class="fas fa-download fas-board"></i>
+                </div>
+                <h6 class="board-h6">Easy Download</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo qui obcaecati beatae minus a</p>
+              </div>
+            </div>
+            <div class="col-md-4 col-lg-4 col-sm-12">
+              <div class="board-main">
+                <div class="board-icon">
+                  <i class="fas fa-pencil-ruler fas-board"></i>
+                </div>
+                <h6 class="board-h6">Cool Templates</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo qui obcaecati beatae minus a</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+    <!-- CREATE SECTION -->
+     <section class="create">
+       <div class="container">
+         <div class="create-div">
+            <h6 class="create-h6">Be Creative</h6>
+            <p class="create-p">Create your Resume the Right way with OctaJobs</p>
+            <a href="" class="btn btn-create" href="#">Create Resume</a>
+         </div>
+       </div>
+     </section> 
+     
+    <!-- WHAT TO KNOW SECTION -->
+     <section class="what-to">
+       <div class="container">
+         <div class="row what-to-div">
+           <div class="col-lg-6 col-md-6 col-sm-12">
+             <img src="/assets/img/what-do.svg" class="img-fluid img-what-to" alt="What To">
+           </div>
+           <div class="col-lg-6 col-md-6 col-sm-12">
+             <div class="what-to-do">
+               <h3 class="what-h3">How Does It Work?</h3>
+               <h6 class="what-head"><i class="fas fa-dot-circle fas-what"></i>  It's pretty easy</h6>
+               <div class="what-list">
+                <p class="what-p"><i class="fas fa-angle-double-right"></i> You will get a URL for every CV you create</p>
+                 <p class="what-p"><i class="fas fa-angle-double-right"></i> Among more than 20 templates and counting</p>
+                 <p class="what-p"><i class="fas fa-angle-double-right"></i> Or download your cv in PDF or .doc</p>
+               </div>
+               <h6 class="what-head"><i class="fas fa-dot-circle fas-what"></i>  It's pretty easy</h6>
+               <div class="what-list">
+                <p class="what-p"><i class="fas fa-angle-double-right"></i> You will get a URL for every CV you create</p>
+                 <p class="what-p"><i class="fas fa-angle-double-right"></i> Among more than 20 templates and counting</p>
+                 <p class="what-p"><i class="fas fa-angle-double-right"></i> Or download your cv in PDF or .doc</p>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </section>
+
+    <!-- FOOTER -->
+     <footer class="footer">
+       <div class="container">
+         <div class="row footer-div">
+           <div class="col-lg-3 col-md-12 col-sm-12">
+             <div class="img-div">
+
+             </div>
+             <img class="logo-footer img-fluid" src="/assets/img/OctaJobs.svg" alt="Logo">
+           </div>
+           <div class="col-lg-6 col-md-12 col-sm-12 footer-link">
+             <a href="#" class="btn-footer">Home</a>
+             <a href="#" class="btn-footer">Create Resume</a>
+             <a href="#" class="btn-footer">Resume Template</a>
+             <a href="#" class="btn-footer">Jobs Listing</a>
+           </div>
+           <div class="col-lg-3 col-md-12 col-sm-12">
+             <p class="copyright" >© OctaJobs, 2020.</p>
+            </div>
+         </div>
+       </div>
+     </footer>
+
 
     <!-- Link to JS -->
     <script src="/assets/js/jquery.min.js"></script>
     <script src="/assets/js/bootstrap.min.js"></script>
     <script src="/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/parallax.min.js"></script>
     <script src="/assets/js/app.js"></script>
 </body>
 </html>
